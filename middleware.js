24d67e23457c623531749676a6b73d11b8c07e65
Nosiris90 <nike.djateng@gmail.com>
2025-05-23 12:03:11 -0400

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,16 +1,14 @@
 // middleware.js
 import { clerkMiddleware } from '@clerk/nextjs/server'
 
+console.log("✅ Middleware Clerk exécuté")
+
 export default clerkMiddleware()
 
 export const config = {
   matcher: [
-    /*
-     * Applique Clerk à toutes les pages sauf :
-     * - fichiers statiques
-     * - APIs publiques
-     * - page racine
-    */
-    "/((?!.*\\..*|_next|sign-in|sign-up|api/public|favicon.ico|$).*)",
+    // Applique Clerk à toutes les pages sauf :
+    // fichiers statiques, sign-in, sign-up, APIs publiques, favicon
+    "/((?!.*\\..*|_next|sign-in|sign-up|api/public|favicon.ico).*)",
   ],
 }
